c8992df0967acf2d729c0a69007a94179c638332
maryaamox@gmail.com <joeyneutron>
2023-07-10 15:51:13 +0100

    yes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,4 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import "../css/App.css";
-import logo from "../images/logo.png";
 
-function Header() {
-  const [isHeaderSticky, setIsHeaderSticky] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.pageYOffset;
-      const header = document.querySelector(".header");
-      const headerHeight = header.offsetHeight;
-
-      if (scrollPosition >= headerHeight) {
-        setIsHeaderSticky(true);
-      } else {
-        setIsHeaderSticky(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  return (
-    <div className={`header ${isHeaderSticky ? "sticky" : ""}`}>
-      <img className="headerImg" src={logo} alt="Logo" />
-      <div className="title">
-        <h1>How Can We Help?</h1>
-      </div>
-    </div>
-  );
-}
-
-export default Header;
-=======
 import React from "react";
 import header from "../images/faqheader.jpg";
 
@@ -50,4 +11,3 @@ function Header(){
 }
 
 export default Header;
->>>>>>> e71783d (changes)
